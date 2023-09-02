568b6cecee88345f3f1ad8bc7e0b04d51dcf1a89
prathamdupare <prathamdupare@proton.me>
2023-09-02 16:01:11 +0530

    added some more functionality

--- src/index.js
+++ src/index.js
@@ -3,14 +3,29 @@ import { Createhome } from "./modules/home";
 import { addTaskDiv, addBarToMainframe } from "./modules/mainframe";
 import { addCategories, createCategories, addBarToCategories, addNewCategories } from "./modules/categories";
 import { createDialogBox } from "./modules/categories";
+import { addNameCategory } from "./modules/categories";
+import { addTaskSpace } from "./modules/categories";
 
-Createhome();
-createCategories();
-addBarToCategories();
-addBarToMainframe()
+document.addEventListener('DOMContentLoaded', () => {
+    // Call functions to set up the app
+    Createhome();
+    createCategories();
+    addBarToCategories();
+    addBarToMainframe();
 
-// addNewCategories();
+    // Add event listener for the category button
+    const categoryButton = document.querySelector('.add-button');
+    categoryButton.addEventListener('click', createDialogBox);
 
-
-const categoryButton = document.querySelector('.add-button');
-categoryButton.addEventListener('click', createDialogBox);
+    // Select all elements with the class .new-category and add click event listeners
+    const newCategories = document.querySelectorAll('.new-category');
+    newCategories.forEach(newCategory => {
+        newCategory.addEventListener('click', () => {
+            // Get the text content of the clicked element (category name)
+            const categoryName = newCategory.innerText;
+            
+            // Call the function to add a task space with the category name
+            addTaskSpace(categoryName);
+        });
+    });
+});
