9e3e4368942314eed89395aa49c85cf40777bc1e
prathamdupare <prathamdupare@proton.me>
2023-09-04 16:51:25 +0530

    Added Tasks and logic

--- src/index.js
+++ src/index.js
@@ -6,26 +6,35 @@ import { createDialogBox } from "./modules/categories";
 import { addNameCategory } from "./modules/categories";
 import { addTaskSpace } from "./modules/categories";
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Call functions to set up the app
-    Createhome();
-    createCategories();
-    addBarToCategories();
-    addBarToMainframe();
 
-    // Add event listener for the category button
-    const categoryButton = document.querySelector('.add-button');
-    categoryButton.addEventListener('click', createDialogBox);
+import { addCategoriesForExistingItems } from "./modules/categories";
 
-    // Select all elements with the class .new-category and add click event listeners
-    const newCategories = document.querySelectorAll('.new-category');
-    newCategories.forEach(newCategory => {
-        newCategory.addEventListener('click', () => {
-            // Get the text content of the clicked element (category name)
-            const categoryName = newCategory.innerText;
-            
-            // Call the function to add a task space with the category name
-            addTaskSpace(categoryName);
-        });
+Createhome();
+createCategories();
+addBarToCategories();
+addBarToMainframe();
+addCategoriesForExistingItems();
+
+// Add event listener for the category button
+const categoryButton = document.querySelector('.add-button');
+categoryButton.addEventListener('click', createDialogBox);
+
+
+
+
+function reCheck() {
+  document.addEventListener('DOMContentLoaded', function () {
+    const uniqueCategories = document.querySelectorAll('.new-category');
+    uniqueCategories.forEach(function (uniqueCategory) {
+      const uniqueCategoryId = uniqueCategory.id;
+      uniqueCategory.addEventListener('click', function () {
+        addTaskSpace(uniqueCategoryId);
+      });
     });
-});
+  });
+}
+
+
+
+
+
---
+++ src/modules/taskspace.js
@@ -0,0 +1,6 @@
+const taskInput = document.querySelector('.task-input');
+const addTaskButton = document.querySelector('.space-button');
+
+
+taskText = taskInput.textContent;
+ulTasls = document.querySelector('Notes');
